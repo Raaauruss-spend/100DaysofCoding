203b4dab46fbcd0006c2010f06c8c0b6eb630f79
Raaauruss-spend <muspiraindah16@gmail.com>
2026-09-02 23:21:30 +0800

    Create Day1.java
    Comment (Komentar)

---
+++ Day1.java
@@ -0,0 +1,21 @@
+public class Day1 {
+  public static void main(String[] args) {
+
+/*
+Komentar (comment)
+memberikan catatan atau keterangan tertulis yang diabaikan oleh mesin saat program dijalankan. 
+*/
+
+//Single-Line Comment (digunakan untuk kalimat satu baris/catatan singkat saja)
+
+/*
+Multi-Line Comment 
+Digunakan untuk penjelasan yang lebih panjang
+atau menonaktifkan blok kode sementara
+*/
+    
+    
+    System.out.println("Hello World!");
+  }
+}
+
