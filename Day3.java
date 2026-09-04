d6c9d802d6f77d6972e2f92773c66b639770e677
Raaauruss-spend <muspiraindah16@gmail.com>
2026-09-04 12:17:32 +0800

    Create Day3.java
    Penggunaan Escape Sequence (\n, \t, dan lain lain).

---
+++ Day3.java
@@ -0,0 +1,30 @@
+public class Day3 {
+  public static void main(String[] args) {
+
+ // Penggunaan Escape Sequence (\n, \t, dan lain lain).
+
+ //Escape sequence dalam Java adalah kombinasi karakter khusus yang diawali dengan tanda garis miring terbalik (\) untuk menampilkan karakter khusus atau mengatur format teks di dalam string.
+
+//Beberapa contohnya:
+
+// Menggunakan \n (Newline) untuk baris baru
+System.out.println("Pira\nIndah");
+
+System.out.println("");
+    
+// Menggunakan \t untuk tab
+System.out.println("Nama:\tMuspira Indah");
+
+System.out.println("");
+
+// Menggunakan \" (Double Quote) untuk kutip ganda
+System.out.println("Naruto berteriak, \"Aku akan menjadi Hokage!\"");
+
+System.out.println("");
+
+// Menggunakan \' (Single Quote) untuk kutip tunggal 
+System.out.println("Rimuru Tempest dikenal sebagai Slime yang membawa \'kedamaian\', meskipun kekuatannya setara dewa penghancur.");
+
+ 
+  }
+}
