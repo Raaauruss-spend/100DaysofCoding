4db64464a0ed68a9da55880096922a9606ba3ffa
Raaauruss-spend <muspiraindah16@gmail.com>
2026-09-07 17:30:00 +0800

    Create Day6.java
    Tipe Data Karakter (char) dan Boolean (boolean).

---
+++ Day6.java
@@ -0,0 +1,33 @@
+public class Day6 {
+  public static void main(String[] args) {
+
+  //Tipe Data Karakter (char) dan Boolean (boolean).
+
+/* Tipe Data Karakter (char)
+   char digunakan untuk menyimpan satu karakter tunggal — bisa huruf, angka (sebagai simbol), atau simbol lain. Aturannya:
+Ditulis pakai tanda kutip satu (' '), bukan kutip dua
+*/
+
+  //Example:
+
+  char JenisKelamin = 'P';
+
+  System.out.println("Jenis Kelamin : " + JenisKelamin);
+  //Outputnya berupa "Jenis Kelamin : P"
+
+  System.out.println("");
+
+
+  /*Tipe Data Boolean (boolean)
+    boolean cuma punya dua kemungkinan nilai: true atau false
+  */
+
+  //Example:
+
+    boolean Otaku = true;
+
+    System.out.println("Dia suka anime : " + Otaku);
+
+    
+  }
+}
