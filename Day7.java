7fd5ee7a680ecf76e432e5f40164e03ce118c85c
Raaauruss-spend <muspiraindah16@gmail.com>
2026-09-08 21:58:24 +0800

    Create Day7.java
    Pengenalan Tipe Data String untuk teks.

---
+++ Day7.java
@@ -0,0 +1,28 @@
+public class Day7 {
+  public static void main(String[] args) {
+
+
+//Pengenalan Tipe Data String untuk teks.
+
+//String — Kumpulan karakter/teks.
+
+
+// Example:
+
+// Anime Weathering with You (Tenki no Ko)
+
+String dialogA = "Hina! Let's go home!";
+String dialogB = "But if I don't... the weather...";
+String dialogC = "Hina! You don't have to be the Sunshine Girl anymore! Forget about the weather! It can stay crazy forever! It doesn't matter if it never stops raining!";
+String dialogD = "Who cares if we never see the sun again?! I want you more than any blue-sky! The weather can stay crazy";
+String dialogE = "I just want you, Hina. I want us to be together!";
+  
+  
+System.out.println("Hodaka : " + dialogA );
+System.out.println("Hina   : " + dialogB);
+System.out.println("Hodaka : " + dialogC);
+System.out.println("Hodaka : " + dialogD);
+System.out.println("Hodaka : " + dialogE);
+    
+  }
+}
