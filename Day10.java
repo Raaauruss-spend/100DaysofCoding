0c719f03a2b08fbe7e0e3199a7f83d33a3e633c1
Raaauruss-spend <muspiraindah16@gmail.com>
2026-09-11 22:32:50 +0800

    Create Day10.java
    Pemahaman Batas Minimal dan Maksimal dari tipe data

---
+++ Day10.java
@@ -0,0 +1,21 @@
+public class Day10 {
+    public static void main(String[] args) {
+
+      
+      //Bilangan bulat (integer)
+System.out.println("byte  : " + Byte.MIN_VALUE + " s/d " + Byte.MAX_VALUE);
+System.out.println("short : " + Short.MIN_VALUE + " s/d " + Short.MAX_VALUE);
+System.out.println("int   : " + Integer.MIN_VALUE + " s/d " + Integer.MAX_VALUE);
+System.out.println("long  : " + Long.MIN_VALUE + " s/d " + Long.MAX_VALUE);
+
+      // Pecahan dan Karakter 
+System.out.println("float  : " + Float.MIN_VALUE + " s/d " + Float.MAX_VALUE);
+System.out.println("double : " + Double.MIN_VALUE + " s/d " + Double.MAX_VALUE);
+System.out.println("char   : " + (int) Character.MIN_VALUE + " s/d " + (int) Character.MAX_VALUE);
+    
+    
+
+
+     
+    }
+}
