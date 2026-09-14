1a338aac2a5209e1d2d727fae2c11475e120a1b2
Raaauruss-spend <muspiraindah16@gmail.com>
2026-09-14 22:54:28 +0800

    Create Day13.java
    Latihan

---
+++ Day13.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Day13 {
+  public static void main(String[] args) {
+
+    Scanner a = new Scanner(System.in);
+
+    String b = a.nextLine();
+    System.out.print("No HP : " + b);
+    
+  }
+}
