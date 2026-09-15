a83c225440b5e1f81b28b7906fefc3d68b11c22b
Raaauruss-spend <muspiraindah16@gmail.com>
2026-09-15 23:09:00 +0800

    Create Day14.java
    Operator Aritmatika Penjumlahan (+) dan Pengurangan (-)

---
+++ Day14.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; 
+
+public class Day14 {
+  public static void main(String[] args) {
+
+  //Operator Aritmatika Penjumlahan (+) dan Pengurangan (-).
+    
+  Scanner in = new Scanner(System.in);
+    
+  int a = in.nextInt();
+  int b = in.nextInt();
+
+
+  System.out.println("Hasil Penjumlahan : " + ( a + b ));
+  System.out.println("Hasil Pengurangan : " + ( b - a ));
+    
+    
+  }
+}
