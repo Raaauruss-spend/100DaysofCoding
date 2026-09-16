ee33ef27353ea636e1267d9bdbeaecd11895cf39
Raaauruss-spend <muspiraindah16@gmail.com>
2026-09-16 23:17:32 +0800

    Create Day15.java
    Operator Aritmatika Perkalian (*) dan Pembagian (/).

---
+++ Day15.java
@@ -0,0 +1,22 @@
+import java.util.Scanner; 
+
+public class Day15 {
+  public static void main(String[] args) {
+
+  //Operator Aritmatika Perkalian (*) dan Pembagian (/).
+    
+  Scanner in = new Scanner(System.in);
+    
+  int a = in.nextInt();
+  int b = in.nextInt();
+
+  int c = a * b ;
+  int d = a / b ;
+
+
+  System.out.println("Hasil Perkalian : " + c);
+  System.out.println("Hasil Pembagian : " + d);
+    
+    
+  }
+}
