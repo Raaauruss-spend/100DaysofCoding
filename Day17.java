bde210fcc161ab641584a9f5f29f6e752512c242
Raaauruss-spend <muspiraindah16@gmail.com>
2026-09-18 21:49:18 +0800

    Create Day17.java
    Operator Penugasan (=, +=, -=, dll).

---
+++ Day17.java
@@ -0,0 +1,23 @@
+public class Day17 {
+    public static void main(String[] args) {
+
+    // Operator Penugasan (=, +=, -=, dll).
+      
+        int a = 10;
+
+        a += 5;
+        System.out.println("Setelah += : " + a);
+
+        a -= 3;
+        System.out.println("Setelah -= : " + a);
+
+        a *= 2;
+        System.out.println("Setelah *= : " + a);
+
+        a /= 4;
+        System.out.println("Setelah /= : " + a);
+
+        a %= 3;
+        System.out.println("Setelah %= : " + a);
+    }
+}
