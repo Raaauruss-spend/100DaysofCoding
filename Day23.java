d035a6e7cf89701c8dc30ea0e922ff053447503a
Raaauruss-spend <muspiraindah16@gmail.com>
2026-09-24 23:18:58 +0800

    Day23.java
    Latihan: Program Menghitung Luas Persegi.

---
+++ Day23.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Day23 {
+    public static void main(String[] args) {
+
+    // Latihan: Program Menghitung Luas Persegi.
+      
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan sisi persegi: ");
+        double sisi = input.nextDouble();
+
+        double luas = sisi * sisi;
+
+        System.out.println("Luas persegi = " + luas);
+    }
+                                    }
